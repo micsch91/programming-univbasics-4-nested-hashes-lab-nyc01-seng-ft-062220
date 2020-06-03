2359cc6e784c81c805001aff4c90ff4227f9de80
Michael Schneider <mic.sch91@gmail.com>
2020-06-03 01:00:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,6 +82,13 @@ def adding_matz
       }
     }
 
+programmer_hash[:yukihiro_matsumoto][:known_for => "Ruby"][:languages => "LISP", "C"]
+
+		 :yukihiro_matsumoto => {
+		   :known_for => "Ruby",
+		   :languages => ["LISP", "C"]
+		 }
+
 end
 
 def changing_alan
